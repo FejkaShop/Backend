528deaf15a14f2a19c2daebf6e891473ba6f4b1e
rcMarty <korotwitschkamartin@gmail.com>
2024-10-19 19:08:13 +0200

    added format ci and lint

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 
 import productRouter from './routers/products';
-import path from "node:path";
+import path from 'node:path';
 
 const app = express();
 const prisma = new PrismaClient();
@@ -12,9 +12,9 @@ const prisma = new PrismaClient();
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routers
 app.use('/products', productRouter);
@@ -22,5 +22,5 @@ app.use('/products', productRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
