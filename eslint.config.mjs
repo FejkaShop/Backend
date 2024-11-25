f979377f121af65c4123b2077da158ce5a489db0
rcMarty <korotwitschkamartin@gmail.com>
2024-11-25 17:05:53 +0100

    eslint now only warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,38 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+// import eslint from '@eslint/js';
+// import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended);
+// export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended);
+
+
+
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...eslint.configs.recommended.rules,
+      ...(tseslint.configs['eslint-recommended'].overrides?.[0]?.rules || {}),
+      ...tseslint.configs.recommended.rules,
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-expressions": "warn",
+    },
+  },
+];
